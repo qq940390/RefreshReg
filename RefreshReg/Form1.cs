9fd3ff4b3b853b6ba649a7fd53b9045b5103dd89
jinhai <940390@qq.com>
2019-10-23 10:09:48 +0800

    添加文件

---
+++ RefreshReg/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RefreshReg
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("shell32.dll")]
+        public static extern void SHChangeNotify(uint wEventId, uint uFlags, IntPtr dwItem1, IntPtr dwItem2);
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SHChangeNotify(0x8000000, 0, IntPtr.Zero, IntPtr.Zero);
+            MessageBox.Show("注册表刷新成功！");
+        }
+    }
+}
